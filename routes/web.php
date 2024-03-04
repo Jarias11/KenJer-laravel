cedfc50a3926cc1a15fa348f821e03f0ddf0e6fb
milton desktop <miltonflores0@gmail.com>
2024-03-03 23:33:59 -0500

    finished footer for now, need to merge to further discuss

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ServicesController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +20,11 @@ use App\Http\Controllers\ServicesController;
 // Home Page
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+// Services Page
 Route::get('/services', [ServicesController::class,'index'])->name('services');
 
+// Gallery Page
+Route::get('/gallery', [GalleryController::class, 'index'])->name('gallery');
+
+//Contact Page
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
