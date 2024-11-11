a320ac026fb9f8ce05081d3e184c25954d17f3d7
Jarias11 <136870799+Jarias11@users.noreply.github.com>
2024-11-11 17:43:22 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TagsTableSeeder::class,
             ImgsTableSeeder::class,
+            AdminUserSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
